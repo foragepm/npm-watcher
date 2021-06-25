b347938a2ea7327a54344e4e7021f21a6ff71144
Andrew Nesbitt <andrewnez@gmail.com>
2021-06-25 15:24:14 +0100

    Hello world

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const ChangesStream = require("@npmcorp/changes-stream");
+const fetch = require('node-fetch');
+
+const changes = new ChangesStream({
+  db: 'https://replicate.npmjs.com/registry',
+  include_docs: true,
+  since: 'now'
+});
+
+changes.on('data', async function(change) {
+  if(change.doc.name){
+    var url = `https://forage-index.herokuapp.com/packages/import?platform=Npm&name=${change.doc.name}&api_key=${process.env.IMPORT_API_KEY}`
+    var res = await fetch(url, {method: 'POST'});
+    console.log(change.doc.name, res.status)
+  }
+})
+
+changes.on('error', function(error){
+  console.error("Failed to connect to the changes feed")
+})
